2d3ae45225f0e48af9992fd45cb96c0a794fab69
Vladimir Moshchuk <ardonplay@gmail.com>
2023-11-04 01:10:31 +0300

    fix: fix fix fix

--- lr2/db/init.sql
+++ lr2/db/init.sql
@@ -1,18 +1,18 @@
 SET datestyle TO 'ISO, DMY';
-CREATE TYPE "product_type" AS ENUM (
-    'industrial',
-    'household',
-    'commerce'
+CREATE TABLE "product_type"(
+    "id" SERIAL PRIMARY KEY,
+    "type" VARCHAR NOT NULL
 );
 
-CREATE TYPE "person_type" AS ENUM ( 'individual', 'legal' );
+CREATE TYPE "person_type" AS ENUM ( 'INDIVIDUAL', 'LEGAL' );
+
 
 CREATE TABLE
     "products" (
         "id" SERIAL PRIMARY KEY,
-        "price_id" integer UNIQUE,
         "name" varchar NOT NULL,
-        "type" product_type NOT NULL
+        "type" int NOT NULL,
+        FOREIGN KEY ("type") REFERENCES "product_type" ("id") ON DELETE CASCADE
     );
     
 
@@ -44,18 +44,10 @@ CREATE TABLE
     "waybills" (
         "id" SERIAL PRIMARY KEY,
         "customer_id" uuid NOT NULL,
+        "date" date NOT NULL,
         "destination" integer NOT NULL,
         FOREIGN KEY ("customer_id") REFERENCES "customers" ("id") ON DELETE CASCADE,
         FOREIGN KEY ("destination") REFERENCES "destinations" ("id") ON DELETE CASCADE
-    );
-
-CREATE TABLE
-    "price_and_quantity" (
-        "id" SERIAL PRIMARY KEY,
-        "date" date NOT NULL,
-        "price" BIGINT NOT NULL,
-        "quantity" integer NOT NULL,
-        FOREIGN KEY ("id") REFERENCES "products" ("price_id") ON DELETE CASCADE
     );
 
 CREATE TABLE
@@ -63,30 +55,34 @@ CREATE TABLE
         "id" SERIAL PRIMARY KEY,
         "product_id" integer NOT NULL,
         "waybil_id" integer NOT NULL,
+        "quantity" integer NOT NULL,
+        "price" BIGINT NOT NULL,
         FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON DELETE CASCADE,
         FOREIGN KEY ("waybil_id") REFERENCES "waybills" ("id") ON DELETE CASCADE
     );
 
 
-INSERT INTO "products" ("price_id", "name", "type") VALUES
-(1, 'Холодильник ATLANT ХМ 4626-109 ND', 'household'),
-(2, 'Ленточнопильный станок Stalex BS-85', 'industrial'),
-(3, 'Компактный банкомат NCR SelfServ 22', 'commerce'),
-(4, 'Блок питания Deepcool PF600 (R-PF600D-HA0B-EU) 600W, APFC, 80 Plus', 'household'),
-(5, 'Посудомоечная машина Electrolux EEA12100L', 'household');
+CREATE VIEW waybil_prices AS
+SELECT waybil_id, SUM(price) AS total_price
+FROM waybil_products
+GROUP BY waybil_id;
 
-INSERT INTO "price_and_quantity" ("date", "price", "quantity") VALUES
-('27/10/2023', 1553, 10),
-('26/10/2023', 9593.78,3),
-('25/10/2023', 7564.22, 7),
-('24/09/2023', 154.90, 156),
-('30/08/2023', 1510.30, 6);
+INSERT INTO "product_type" ("type") VALUES
+('INDUSTRIAL'),
+('HOUSEHOLD'),
+('COMMERCE');
 
+INSERT INTO "products" ("name", "type") VALUES
+('Холодильник ATLANT ХМ 4626-109 ND', 2),
+('Ленточнопильный станок Stalex BS-85', 1),
+('Компактный банкомат NCR SelfServ 22', 3),
+('Блок питания Deepcool PF600 (R-PF600D-HA0B-EU) 600W, APFC, 80 Plus', 2),
+('Посудомоечная машина Electrolux EEA12100L', 2);
 
 INSERT INTO "customers" ("name", "type", "adress", "phone_number") VALUES
-('Иванов Иван', 'individual', 'ул. Ленина, 123', '1234567890'),
-('Владимир Мощук', 'individual', 'ул. Кунцевщина, 38', '298277252'),
-('ЧП Сепия', 'legal', 'ул. Притыцкого, 105 — 194', '296291276');
+('Иванов Иван', 'INDIVIDUAL', 'ул. Ленина, 123', '1234567890'),
+('Владимир Мощук', 'INDIVIDUAL', 'ул. Кунцевщина, 38', '298277252'),
+('ЧП Сепия', 'LEGAL', 'ул. Притыцкого, 105 — 194', '296291276');
 
 INSERT INTO "bank_details" ("customer_id", "number", "bank_name")
 VALUES  ((SELECT customers.id FROM "customers" LIMIT 1 OFFSET 0), '1234567890', 'Bank of Example'),
@@ -99,12 +95,14 @@ INSERT INTO "destinations" ("name", "region", "country") VALUES
 
 
 
-INSERT INTO "waybills" ("customer_id", "destination") VALUES
-((SELECT customers.id FROM "customers" LIMIT 1 OFFSET 0),1),
-((SELECT customers.id FROM "customers" LIMIT 1 OFFSET 1),1),
-((SELECT customers.id FROM "customers" LIMIT 1 OFFSET 2),1);
+INSERT INTO "waybills" ("date", "customer_id", "destination") VALUES
+('27/10/2023', (SELECT customers.id FROM "customers" LIMIT 1 OFFSET 0),1),
+('25/10/2023', (SELECT customers.id FROM "customers" LIMIT 1 OFFSET 1),1),
+('30/08/2023',(SELECT customers.id FROM "customers" LIMIT 1 OFFSET 2),1);
 
-INSERT INTO "waybil_products" ("product_id", "waybil_id") VALUES
-(1, 1),
-(2, 2),
-(3,3);
+
+INSERT INTO "waybil_products" ("product_id", "waybil_id", "quantity",
+  "price") VALUES
+(1, 1, 10, 1553),
+(2, 2, 2, 703),
+(3,3, 5, 586);
